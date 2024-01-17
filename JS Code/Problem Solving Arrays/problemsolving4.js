521852af883d6f0655e365fa4c8af982952d5359
Rajat Sharma <rajatxd007@gmail.com>
2024-01-17 21:33:15 +0530

    Added reference codes

--- JS Code/Problem Solving Arrays/problemsolving4.js
+++ JS Code/Problem Solving Arrays/problemsolving4.js
@@ -12,4 +12,28 @@ output : [5,6]
 //If extra space is provided then we can store the array in object and we can check from nunbers from 1 to n whether they are present in object or not
 
 
-//Now we can't use extra space , so we will use the constraint that every number is 
+//Now we can't use extra space , so we will use the constraint that every number is positive
+
+//we will try to exploit the n-1 index so that we can remember that it has done by n-1 only
+
+//nums [4,3,2,7,8,2,3,1]
+//for every number exploit n-1 index
+//exploited array = [-4,-3,-2,-7,8,2,-3,-1]
+
+var findDisappearedNumbers = function(nums) {
+    let resultlist = [];
+    //so first we will exploit the array
+    for(let i = 0 ; i<nums.length;i++){
+        let value = Math.abs(nums[i]);
+        nums[value-1]= Math.abs(nums[value-1])*(-1); 
+    }
+    for(let j =0;j<nums.length;j++){
+        if(nums[j]>0){
+            resultlist.push(j+1);
+        }
+    }
+    console.log(resultlist);
+};
+
+
+findDisappearedNumbers([4,3,2,7,8,2,3,1]);
---
+++ LeetCode/leetcode-791.js
@@ -0,0 +1,65 @@
+/*
+You are given two strings order and s. All the characters of order are unique and were sorted in some custom order previously.
+
+Permute the characters of s so that they match the order that order was sorted. More specifically, if a character x occurs before a character y in order, then x should occur before y in the permuted string.
+
+Return any permutation of s that satisfies this property.
+
+ 
+
+Example 1:
+
+Input: order = "cba", s = "abcd"
+Output: "cbad"
+Explanation: 
+"a", "b", "c" appear in order, so the order of "a", "b", "c" should be "c", "b", and "a". 
+Since "d" does not appear in order, it can be at any position in the returned string. "dcba", "cdba", "cbda" are also valid outputs.
+Example 2:
+
+Input: order = "cbafg", s = "abcd"
+Output: "cbad"
+ 
+
+Constraints:
+
+1 <= order.length <= 26
+1 <= s.length <= 200
+order and s consist of lowercase English letters.
+All the characters of order are unique.
+*/
+
+function custom(order,s){
+    let obj ={};
+    let permutedString='';
+    let remStr = '';
+
+    //for calculating freq
+    for(let i =0;i<s.length;i++){
+        if(obj[s[i]]){
+            obj[s[i]]+=1;
+        }else{
+            obj[s[i]]=1;
+        }
+    }
+    //updating the object and finding the permuted String
+    for(let i=0;i<order.length;i++){
+        if(obj[order[i]]){
+            while(obj[order[i]]>0){
+                obj[order[i]]-=1;
+                permutedString += order[i];
+            }
+            
+        }
+    }
+    //adding the remaining characters from s
+    for(let key in obj){
+        while(obj[key]>0){
+            remStr +=key;
+            obj[key]-=1;
+        }
+    }
+    return permutedString+remStr;
+}
+
+
+console.log(custom('kqep','pekeq'));
